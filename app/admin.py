22753692a0e4102b357e649d17bfa6a5e655f73a
gege <nemeth.n.gergo@gmail.com>
2026-03-01 20:04:09 +0100

    create photo model and admin page

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from app.models import Photo
+
 # Register your models here.
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'photo', 'uploaded_at', 'owner')
+    list_filter = ('uploaded_at', 'owner')
+    search_fields = ('name', 'owner__username')
+
+admin.site.register(Photo, PhotoAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Photo(models.Model):
+    name = models.CharField(max_length=60)
+    photo = models.ImageField(upload_to='media/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey('auth.User', related_name='photos', on_delete=models.CASCADE)
