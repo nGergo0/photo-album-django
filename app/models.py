4d1751f90c7dc6edd20d0703af5a2db1c0d9d7b7
gege <nemeth.n.gergo@gmail.com>
2026-03-01 20:16:59 +0100

    update model

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-# Create your models here.
 class Photo(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=40)
     photo = models.ImageField(upload_to='media/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey('auth.User', related_name='photos', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
